b14722f93eae748ddf12043f9c4532e3e6e0c348
Kunal <kunalkumaarsingh@gmail.com>
2022-08-21 19:34:27 +0530

    added routing for microforntend apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
-export default  () =>{
-    return(<div>
-         <h1>Hello Container</h1>
-         <hr></hr>
-         <MarketingApp />
-    </div>)
+export default () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>)
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
+const mount = (el, {onNavigate, defaultHistory}) => {
+    const history = defaultHistory || createMemoryHistory({ initialEntries: [window.location.pathname]});
+
+    if(onNavigate){
+        console.log(history)
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     )
+
+    return {
+        onParentNavigate: (location) => {
+            const { pathname: nextPathname } = location;
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector("#_marketing_dev_root");
 
     if (devRoot) {
-        mount(devRoot)
+        mount(devRoot, {defaultHistory: createBrowserHistory()})
     }
 
 }
